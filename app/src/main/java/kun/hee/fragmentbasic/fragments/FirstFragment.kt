4daf3e05aa0209b5b75c9545534391d791aa2982
smlrkh1107 <krae7789230@gmail.com>
2020-04-16 22:04:48 +0900

    프래그먼트 클래스 / XML 생성.

---
+++ app/src/main/java/kun/hee/fragmentbasic/fragments/FirstFragment.kt
@@ -0,0 +1,8 @@
+package kun.hee.fragmentbasic.fragments
+
+import androidx.fragment.app.Fragment
+
+//최신버전이므로 android X 잇는걸루다가
+class FirstFragment :Fragment() {
+//    조각나있지만 얘도 화면에 올라갈 애야.ㅎㅎ XML파일 별도로 생성해야해. ~~!
+}
