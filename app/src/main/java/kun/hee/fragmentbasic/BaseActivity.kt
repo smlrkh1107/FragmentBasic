477dbfb1ec793ce2f9834b36a5bf7bc7399aa99a
smlrkh1107 <krae7789230@gmail.com>
2020-04-16 21:50:12 +0900

    Initial commit

---
+++ app/src/main/java/kun/hee/fragmentbasic/BaseActivity.kt
@@ -0,0 +1,11 @@
+package kun.hee.fragmentbasic
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity :AppCompatActivity() {
+    val mContext = this;
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+
+}
---
+++ app/src/main/java/kun/hee/fragmentbasic/MainActivity.kt
@@ -0,0 +1,19 @@
+package kun.hee.fragmentbasic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
